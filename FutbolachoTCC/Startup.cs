89e0d0f97c399f06f8a12912c8776915a33d41b0
Oscarr W Santacruz <oscar.sntcrz@gmail.com>
2021-09-28 23:49:24 -0400

    ajustes de textos

--- FutbolachoTCC/Startup.cs
+++ FutbolachoTCC/Startup.cs
@@ -27,9 +27,9 @@ namespace FutbolachoTCC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string conexionString = "Server=(localdb)\\mssqllocaldb;Database=aspnet-FutbolachoTCC-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true";
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(conexionString));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
